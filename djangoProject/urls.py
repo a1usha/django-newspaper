7e0aef48a242b82d01e48caa4fe370eef1bd7bae
a1usha <aleksandr.ushaev@hotmail.com>
2021-04-26 10:39:30 +0700

    add roles

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -31,6 +31,7 @@ from ckeditor_uploader import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('add_user/', user_views.add_user, name='add-user'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from .models import Profile
+from django.contrib.auth.forms import UserCreationForm
+
 
 # ModelForm allows to create form that will work sith 
 # specific database model
@@ -14,3 +16,16 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
+
+
+class UserForm(UserCreationForm):
+    # group = forms.Select(choices=[
+    #     ('simple_user', 'Simple user'),
+    #     ('copy_editor', 'Copy editor')
+    # ])
+
+    group = forms.ModelChoiceField(queryset=Group.objects.all(), required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'group', 'password1' ,'password2' )
